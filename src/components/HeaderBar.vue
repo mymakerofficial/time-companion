daedf6155d0be497f3989aaf58365758770f7c09
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-01-09 15:57:17 +0100

    added theme toggle

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+import {useDark, useToggle} from "@vueuse/core";
+import {Button} from "@/components/ui/button";
+import {Moon, Sun} from "lucide-vue-next";
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
   <header class="sticky top-0 z-40">
-    <div class="h-14 bg-background border-b border-border px-8 flex flex-row items-center">
-      <span class="font-medium tracking-wide">Time Companion</span>
+    <div class="h-14 bg-background border-b border-border px-8 flex flex-row items-center justify-between">
+      <div>
+        <span class="font-medium tracking-wide">Time Companion</span>
+      </div>
+      <div>
+        <Button @click="toggleDark()" size="icon" variant="ghost">
+          <Moon v-if="isDark" class="size-4" />
+          <Sun v-else class="size-4" />
+        </Button>
+      </div>
     </div>
   </header>
 </template>
