5ab04ae46a737664e800e9cc8cc91980992f8bb2
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-01-09 15:53:00 +0100

    improved visibility of marker

--- src/components/calendar/CalendarViewPointer.vue
+++ src/components/calendar/CalendarViewPointer.vue
@@ -14,7 +14,7 @@ const containerStyle = computed(() => {
 </script>
 
 <template>
-  <div class="col-start-1 col-span-full border-t-2 border-primary z-10" :style="containerStyle">
-    <span class="absolute size-[0.6rem] rounded-full bg-primary -ml-[0.3rem] -mt-[0.35rem]" />
+  <div class="col-start-1 col-span-full h-0.5 bg-primary z-10 border-y border-background/50 box-content" :style="containerStyle">
+    <span class="absolute size-[0.6rem] rounded-full bg-primary -ml-[0.3rem] -mt-[0.225rem]" />
   </div>
 </template>
