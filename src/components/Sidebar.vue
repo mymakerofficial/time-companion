c5b8032166f3cb4b35e18f00a84d535a9b8f5cfe
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-01-16 13:49:15 +0100

    add favicon

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import {Button} from "@/components/ui/button";
-import {Calendar, Clock, Settings} from "lucide-vue-next";
+import {Calendar, Settings} from "lucide-vue-next";
+import logo from '@/assets/logo.png'
 </script>
 
 <template>
   <div class="min-w-16 h-screen border-r border-border">
     <div class="h-16 border-b border-border flex justify-center items-center">
-      <Clock class="size-5" />
+      <img :src="logo" alt="logo" class="h-8" />
     </div>
     <div class="flex flex-col items-center gap-4 py-4">
       <Button variant="secondary" size="icon"><Calendar class="size-5" /></Button>
