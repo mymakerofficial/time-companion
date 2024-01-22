c0c3ba69d68093f853e83a515aaa792c1721fc26
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-01-22 11:37:36 +0100

    feature: add parent project selector to edit activity dialog

--- src/components/settings/projects/dialog/EditActivityDialog.vue
+++ src/components/settings/projects/dialog/EditActivityDialog.vue
@@ -2,12 +2,16 @@
 import BaseDialog from "@/components/common/dialog/BaseDialog.vue";
 import {isNotNull, isNull, type Nullable} from "@/lib/utils";
 import {useProjectsStore} from "@/stores/projectsStore";
-import {computed, reactive, watch} from "vue";
+import {reactive, watch} from "vue";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {useReferenceById} from "@/composables/useReferenceById";
 import ColorSelect from "@/components/common/inputs/colorSelect/ColorSelect.vue";
 import {whenever} from "@vueuse/core";
+import type {ReactiveProject} from "@/model/project";
+import {Slash} from "lucide-vue-next"
+import type {ComboboxOption} from "@/components/common/inputs/combobox/types";
+import Combobox from "@/components/common/inputs/combobox/Combobox.vue";
 
 const props = defineProps<{
   id: Nullable<string>
@@ -20,8 +24,14 @@ const emit = defineEmits<{
 const projectsStore = useProjectsStore()
 const activity = useReferenceById(projectsStore.activities)
 
+const projectOptions: ComboboxOption[] = projectsStore.projects.map((project) => ({
+  value: project.id,
+  label: project.displayName,
+}))
+
 const state = reactive({
   open: true,
+  projectId: null as Nullable<ReactiveProject['id']>,
   displayName: '',
   color: null as Nullable<string>,
 })
@@ -33,6 +43,7 @@ watch(() => props.id, (id) => {
     return
   }
 
+  state.projectId = activity.value.parentProject?.id ?? null
   state.displayName = activity.value.displayName
   state.color = activity.value.color
 }, { immediate: true })
@@ -61,6 +72,13 @@ function handleSubmit() {
   activity.value.displayName = state.displayName
   activity.value.color = state.color
 
+  if (isNotNull(state.projectId) && state.projectId !== activity.value.parentProject?.id) {
+    projectsStore.link(
+      projectsStore.getProjectById(state.projectId),
+      activity.value
+    )
+  }
+
   close()
 }
 </script>
@@ -68,8 +86,11 @@ function handleSubmit() {
 <template>
   <BaseDialog v-model:open="state.open" title="Edit Activity" description="Edit the activity.">
     <div class="flex flex-col gap-4">
-      <div>Parent Project: {{ activity?.parentProject?.displayName }}</div>
-      <Input v-model="state.displayName" placeholder="Name" />
+      <div class="flex flex-row items-center gap-2">
+        <Combobox v-model="state.projectId" :options="projectOptions" />
+        <Slash class="size-4" />
+        <Input v-model="state.displayName" placeholder="Name" />
+      </div>
       <ColorSelect v-model="state.color" />
       <div class="flex flex-row gap-4 justify-between">
         <div class="flex flex-row gap-4">
