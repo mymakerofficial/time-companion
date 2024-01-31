6eb340877b42ccc3117b8379426645683d190add
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-01-31 11:03:39 +0100

    feat: updated project and activity forms

--- src/components/settings/projects/activityDialog/ActivityForm.vue
+++ src/components/settings/projects/activityDialog/ActivityForm.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import Combobox from "@/components/common/inputs/combobox/Combobox.vue";
-import {Input} from "@/components/ui/input";
 import {Slash} from "lucide-vue-next";
-import ColorSelect from "@/components/common/inputs/colorSelect/ColorSelect.vue";
 import type {ComboboxOption} from "@/components/common/inputs/combobox/types";
 import {useProjectsStore} from "@/stores/projectsStore";
 import type {ActivityForm} from "@/components/settings/projects/activityDialog/helpers";
-import Label from "@/components/ui/label/Label.vue";
+import ColorTextInput from "@/components/common/inputs/colorTextInput/ColorTextInput.vue";
 
 defineProps<{
   form: ActivityForm
@@ -25,16 +23,7 @@ const projectOptions: ComboboxOption[] = projectsStore.projects.map((project) =>
     <div class="flex flex-row items-center gap-2">
       <Combobox v-model="form.projectId" :options="projectOptions" :placeholder="$t('dialog.activity.form.parentProject.placeholder')" />
       <Slash class="size-4" />
-      <Input v-model="form.displayName" :placeholder="$t('dialog.activity.form.displayName.placeholder')" />
-    </div>
-    <div class="flex flex-row items-center justify-between rounded-lg border p-4">
-      <div class="space-y-0.5">
-        <Label class="text-base">{{ $t('dialog.activity.form.color.label') }}</Label>
-        <p class="text-sm text-muted-foreground">{{ $t('dialog.activity.form.color.description') }}</p>
-      </div>
-      <div>
-        <ColorSelect v-model="form.color" variant="ghost" />
-      </div>
+      <ColorTextInput v-model:color="form.color" v-model:text="form.displayName" :placeholder="$t('dialog.activity.form.displayName.placeholder')" />
     </div>
   </div>
 </template>
