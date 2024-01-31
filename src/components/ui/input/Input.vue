e86e6dfe2d5bccdaded8c2badcbe3f163847b7a9
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-01-31 11:00:35 +0100

    feat: inputs now have leading and trailing slots

--- src/components/ui/input/Input.vue
+++ src/components/ui/input/Input.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
-import { useVModel } from '@vueuse/core'
-import { cn } from '@/lib/utils'
+import {cn} from '@/lib/utils'
 
-const props = defineProps<{
-  defaultValue?: string | number
-  modelValue?: string | number
-}>()
-
-const emits = defineEmits<{
-  (e: 'update:modelValue', payload: string | number): void
-}>()
-
-const modelValue = useVModel(props, 'modelValue', emits, {
-  passive: true,
-  defaultValue: props.defaultValue,
+const props = withDefaults(defineProps<{
+  type?: 'text' | 'password' | 'email' | 'number' | 'search' | 'tel' | 'url'
+  placeholder?: string
+  class?: string
+  inputClass?: string
+}>(), {
+  type: 'text',
+  class: '',
+  inputClass: '',
 })
+
+const model = defineModel<string | number>({ required: false })
 </script>
 
 <template>
-  <input v-model="modelValue" type="text" :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', $attrs.class ?? '')">
+  <div :class="cn('flex items-center h-10 w-full overflow-hidden rounded-md border border-input bg-background text-sm ring-offset-background placeholder:text-muted-foreground has-[:focus-visible]:ring-2 has-[:focus-visible]:ring-ring has-[:focus-visible]:ring-offset-2', props.class)">
+    <slot name="leading" />
+    <input v-model="model" :type="type" :placeholder="placeholder" :class="cn('h-full w-full first:pl-3 last:pr-3 py-2 bg-transparent  file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50', props.inputClass)">
+    <slot name="trailing" />
+  </div>
 </template>
