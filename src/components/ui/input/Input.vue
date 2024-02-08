a6b56715f383f5f9d37f82bd7da7c96ed1bfba7c
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-02-08 21:13:20 +0100

    feat: input now uses primitive

--- src/components/ui/input/Input.vue
+++ src/components/ui/input/Input.vue
@@ -1,24 +1,47 @@
 <script setup lang="ts">
 import {cn} from '@/lib/utils'
+import {Primitive, type PrimitiveProps} from "radix-vue";
+import {computed, type HTMLAttributes} from "vue";
 
-const props = withDefaults(defineProps<{
-  type?: 'text' | 'password' | 'email' | 'number' | 'search' | 'tel' | 'url'
-  placeholder?: string
-  class?: string
-  inputClass?: string
+const model = defineModel<string | number>({ required: false })
+
+const props = withDefaults(defineProps<Omit<PrimitiveProps, 'asChild'> & {
+  type?: HTMLAttributes['inputmode']
+  placeholder?: HTMLAttributes['placeholder']
+  class?: HTMLAttributes['class']
+  inputClass?: HTMLAttributes['class']
 }>(), {
+  as: 'input',
   type: 'text',
   class: '',
   inputClass: '',
 })
 
-const model = defineModel<string | number>({ required: false })
+const containerProps = computed(() => {
+  const { class: className } = props
+  return {
+    class: cn('flex items-center h-10 w-full overflow-hidden rounded-md border border-input bg-background text-sm ring-offset-background placeholder:text-muted-foreground has-[:focus-visible]:ring-2 has-[:focus-visible]:ring-ring has-[:focus-visible]:ring-offset-2', className)
+  }
+})
+
+const inputProps = computed(() => {
+  const { as, type, placeholder, inputClass } = props
+  return {
+    as,
+    type,
+    placeholder,
+    class: cn('h-full w-full first:pl-3 last:pr-3 py-2 bg-transparent  file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50', inputClass)
+  }
+})
 </script>
 
 <template>
-  <div :class="cn('flex items-center h-10 w-full overflow-hidden rounded-md border border-input bg-background text-sm ring-offset-background placeholder:text-muted-foreground has-[:focus-visible]:ring-2 has-[:focus-visible]:ring-ring has-[:focus-visible]:ring-offset-2', props.class)">
+  <div v-bind="containerProps">
     <slot name="leading" />
-    <input v-model="model" :type="type" :placeholder="placeholder" :class="cn('h-full w-full first:pl-3 last:pr-3 py-2 bg-transparent  file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50', props.inputClass)">
+    <Primitive
+      v-model="model"
+      v-bind="inputProps"
+    />
     <slot name="trailing" />
   </div>
 </template>
