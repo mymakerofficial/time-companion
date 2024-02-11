d0ee85e1b731524258a07abf05b2d6db071d7d2b
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-02-11 17:08:51 +0100

    feat(ProjectActionInput): added model values

--- src/components/common/inputs/projectActionInput/ProjectActionInput.vue
+++ src/components/common/inputs/projectActionInput/ProjectActionInput.vue
@@ -1,20 +1,55 @@
 <script setup lang="ts">
-import {computed, ref} from "vue";
+import {computed, type HTMLAttributes, ref, useAttrs} from "vue";
 import {Input} from "@/components/ui/input";
 import Combobox from "@/components/common/inputs/combobox/Combobox.vue";
 import ComboboxInput from "@/components/common/inputs/combobox/ComboboxInput.vue";
-import {isNotNull, type Nullable} from "@/lib/utils";
+import {cn, isNotDefined, isNotNull, isNull, type Nullable} from "@/lib/utils";
 import {useQuickAccess} from "@/composables/useQuickAccess";
 import {createEventShadow, type ReactiveCalendarEventShadow} from "@/model/calendarEventShadow";
 import ShadowBadge from "@/components/common/shadow/ShadowBadge.vue";
 import {isNotEmpty} from "@/lib/listUtils";
 import {useFocus} from "@vueuse/core";
+import type {ReactiveProject} from "@/model/project";
+import type {ReactiveActivity} from "@/model/activity";
 
-const searchTerm = ref('')
+const projectModel = defineModel<Nullable<ReactiveProject>>('project', { required: false, default: null })
+const activityModel = defineModel<Nullable<ReactiveActivity>>('activity', { required: false, default: null })
+const searchTerm = defineModel<string>('note', { required: false, default: '' })
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+  placeholder?: HTMLAttributes['placeholder']
+}>()
+
+defineOptions({
+  inheritAttrs: false
+})
+
 const input = ref()
 const { focused: inputFocused } = useFocus(input)
 
-const selected = ref<Nullable<ReactiveCalendarEventShadow>>(null)
+const selected = computed<Nullable<ReactiveCalendarEventShadow>>({
+  get() {
+    if (isNotDefined(projectModel.value)) {
+      return null
+    }
+
+    return createEventShadow({
+      project: projectModel.value,
+      activity: activityModel.value,
+    })
+  },
+  set(value) {
+    if (isNull(value)) {
+      projectModel.value = null
+      activityModel.value = null
+      return
+    }
+
+    projectModel.value = value.project
+    activityModel.value = value.activity
+  }
+})
 
 const shadows = useQuickAccess(() => ({
   maxActivitiesPerProject: isNotNull(selected.value) || isNotEmpty(searchTerm.value) ? Infinity : undefined,
@@ -54,6 +89,14 @@ const open = computed(() => {
 
   return true
 })
+
+const placeholder = computed(() => {
+  if (isNotNull(selected.value)) {
+    return null
+  }
+
+  return props.placeholder
+})
 </script>
 
 <template>
@@ -73,6 +116,9 @@ const open = computed(() => {
       <Input
         v-model="searchTerm"
         @keydown.backspace="handleBackspace"
+        :placeholder="placeholder"
+        :class="props.class"
+        v-bind="$attrs"
       >
         <template #leading>
           <ShadowBadge :shadow="selected" variant="default" size="md" class="mx-1.5 rounded-sm" />
