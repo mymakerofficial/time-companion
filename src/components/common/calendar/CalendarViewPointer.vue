d29f449c193e49babaa17312fa35114dfcca775d
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-02-20 19:20:47 +0100

    feat(Calendar): automatically scroll pointer into view

--- src/components/common/calendar/CalendarViewPointer.vue
+++ src/components/common/calendar/CalendarViewPointer.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import {computed} from "vue";
+import {computed, ref, watch} from "vue";
 import {durationToGridRows} from "@/lib/calendarUtils";
 import {durationSinceStartOfDay, minutes} from "@/lib/neoTime";
 import {useNow} from "@/composables/useNow";
+import {useTimeoutFn} from "@vueuse/core";
+
+const pointer = ref<HTMLElement | null>(null)
 
 const now = useNow({ interval: minutes(1) })
 
@@ -10,10 +13,21 @@ const containerStyle = computed(() => {
   const startOffset = 2 // due to spacing at the top
   return { gridRow: durationToGridRows(durationSinceStartOfDay(now.value)) + startOffset }
 })
+
+function scrollIntoView() {
+  pointer.value?.scrollIntoView({
+    block: 'center',
+    behavior: 'smooth',
+  })
+}
+
+watch(now, () => {
+  useTimeoutFn(scrollIntoView, 10)
+}, { immediate: true })
 </script>
 
 <template>
-  <div class="col-start-1 col-span-full h-0.5 bg-primary z-10 border-y border-background/50 box-content" :style="containerStyle">
+  <div ref="pointer" @click="scrollIntoView" class="col-start-1 col-span-full h-0.5 bg-primary z-10 border-y border-background/50 box-content" :style="containerStyle">
     <span class="absolute size-[0.6rem] rounded-full bg-primary -ml-[0.3rem] -mt-[0.225rem]" />
   </div>
 </template>
