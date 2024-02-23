f51a652292d50358f68c466eb9e7a03c6f1d7087
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-02-23 10:51:28 +0100

    chore: added vue devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsxPlugin from "@vitejs/plugin-vue-jsx";
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsxPlugin()
+    vueJsxPlugin(),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
