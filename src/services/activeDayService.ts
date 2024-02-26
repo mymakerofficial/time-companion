a1f9d7a1ae0d65e94afda3def4ef9879b1f37e47
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-02-26 18:52:41 +0100

    feat: set active event when active day is set

--- src/services/activeDayService.ts
+++ src/services/activeDayService.ts
@@ -1,14 +1,15 @@
 import type {ReactiveCalendarDay} from "@/model/calendarDay/types";
 import {Temporal} from "temporal-polyfill";
 import type {Nullable} from "@/lib/utils";
-import {check, isDefined} from "@/lib/utils";
+import {check, isDefined, isNull} from "@/lib/utils";
 import {useActiveDayStore} from "@/stores/activeDayStore";
-import {whereDate} from "@/lib/listUtils";
+import {lastOf, whereDate} from "@/lib/listUtils";
 import {createDay} from "@/model/calendarDay/model";
 import {useCalendarService} from "@/services/calendarService";
 import {reactive} from "vue";
 import {mapReadonly} from "@/model/modelHelpers";
 import {createService} from "@/composables/createService";
+import {useActiveEventService} from "@/services/activeEventService";
 
 export interface ActiveDayService {
   readonly day: Nullable<ReactiveCalendarDay>
@@ -27,10 +28,17 @@ export const useActiveDayService = createService<ActiveDayService>(() => {
     )
 
     activeDayStore.unsafeSetDay(day)
+
+    const lastNonEndedEvent = lastOf(day.events.filter((it) => isNull(it.endAt)))
+
+    if (isDefined(lastNonEndedEvent)) {
+      useActiveEventService().setEvent(lastNonEndedEvent)
+    }
   }
 
   function unsetDay() {
     activeDayStore.unsafeSetDay(null)
+    useActiveEventService().unsetEvent()
   }
 
   function setByDate(date: Temporal.PlainDate) {
