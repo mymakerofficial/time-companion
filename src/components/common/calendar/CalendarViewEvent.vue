ebbb2066f9f20981a74b98c0704d6fa70e3522c7
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-03-08 15:20:31 +0100

    ui: calendar event nowrap

--- src/components/common/calendar/CalendarViewEvent.vue
+++ src/components/common/calendar/CalendarViewEvent.vue
@@ -63,7 +63,7 @@ function handleClick() {
   <div class="flex relative mt-0.5 col-span-full" :style="containerStyle">
     <div @click="handleClick" v-provide-color="event.color" class="cursor-pointer absolute inset-0.5 flex rounded-md overflow-hidden bg-background min-h-3">
       <div class="flex-1 px-2 py-1 flex flex-col gap-1 bg-color text-color-foreground hover:bg-color/90">
-        <div class="text-sm flex items-center gap-2">
+        <div class="text-sm flex items-center gap-2 text-nowrap">
           <div class="font-medium flex items-center gap-1">
             <span>{{ event.projectDisplayName }}</span>
             <Slash v-if="isNotEmpty(event.activityDisplayName)" class="size-3" />
