2d3217b34127133aa542a87b6e161f9e7e51b522
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-03-08 14:57:09 +0100

    ui: improved calendar event text arrangement

--- src/components/common/calendar/CalendarViewEvent.vue
+++ src/components/common/calendar/CalendarViewEvent.vue
@@ -6,6 +6,8 @@ import type {ReactiveCalendarEvent} from "@/model/calendarEvent/types";
 import {durationBetween, durationSinceStartOfDay, formatTime, minutes, withFormat} from "@/lib/neoTime";
 import {isNull} from "@/lib/utils";
 import {useNow} from "@/composables/useNow";
+import {isNotEmpty} from "@/lib/listUtils";
+import {Slash} from "lucide-vue-next";
 
 const props = defineProps<{
   event: ReactiveCalendarEvent
@@ -61,14 +63,15 @@ function handleClick() {
   <div class="flex relative mt-0.5 col-span-full" :style="containerStyle">
     <div @click="handleClick" v-provide-color="event.color" class="cursor-pointer absolute inset-0.5 flex rounded-md overflow-hidden bg-background min-h-3">
       <div class="flex-1 px-2 py-1 flex flex-col gap-1 bg-color text-color-foreground hover:bg-color/90">
-        <div class="flex flex-row justify-between items-center">
-          <h2 :data-small="containerPosition.spanRows < 2" class="space-x-2 text-sm data-[small=true]:text-xs data-[small=true]:-my-2">
-            <span class="font-medium">{{ event.projectDisplayName || event.note || 'Unnamed' }}</span>
-            <span v-if="!(!event.projectDisplayName && !event.activityDisplayName && event.note)">{{ event.activityDisplayName || event.note }}</span>
-          </h2>
+        <div class="text-sm flex items-center gap-2">
+          <div class="font-medium flex items-center gap-1">
+            <span>{{ event.projectDisplayName }}</span>
+            <Slash v-if="isNotEmpty(event.activityDisplayName)" class="size-3" />
+            <span v-if="isNotEmpty(event.activityDisplayName)">{{ event.activityDisplayName }}</span>
+          </div>
+          <span>{{ event.note }}</span>
         </div>
-        <p class="text-xs"><time>{{ startedAtLabel }}</time> - <time>{{ endedAtLabel }}</time></p>
-        <p v-if="event.projectDisplayName && event.activityDisplayName && event.note" class="text-xs">{{ event.note }}</p>
+        <span class="text-xs"><time>{{ startedAtLabel }}</time> - <time>{{ endedAtLabel }}</time></span>
       </div>
     </div>
   </div>
