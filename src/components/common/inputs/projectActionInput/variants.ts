1924723c75ab80772daf5d9d27b59b014b89b4b4
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-03-09 13:36:48 +0100

    ui: unified roundness

--- src/components/common/inputs/projectActionInput/variants.ts
+++ src/components/common/inputs/projectActionInput/variants.ts
@@ -1,13 +1,13 @@
 import {cva} from "class-variance-authority";
 
 export const projectActionInputBadgeVariants = cva(
-  '',
+  'rounded-md',
   {
     variants: {
       size: {
-        sm: 'ml-0.5 mr-2 rounded-sm',
-        md: 'mx-1.5 rounded-sm',
-        lg: 'ml-1 mr-3 rounded-md',
+        sm: 'ml-0.5 mr-2',
+        md: 'mx-1.5',
+        lg: 'ml-1 mr-3',
       },
     },
     defaultVariants: {
