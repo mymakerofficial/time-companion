2831c814ab4ec8566fe7de69ad624958333b70d4
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-03-26 15:03:48 +0100

    feat: improve randomElement

--- src/shared/lib/utils/random.ts
+++ src/shared/lib/utils/random.ts
@@ -2,6 +2,6 @@ export function randomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-export function randomElement<T>(list: Array<T>): T {
+export function randomElement<T>(list: ReadonlyArray<T>): T {
   return list[randomInt(0, list.length - 1)]
 }
