846bef260eaf74f20fe4b44658c28e16a295e9e7
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-03-30 11:53:13 +0100

    feat: moved dao helpers into separate file

--- src/renderer/composables/project/useProjectById.ts
+++ src/renderer/composables/project/useProjectById.ts
@@ -1,185 +1,25 @@
-import type { MaybeRefOrGetter, Ref } from 'vue'
+import type { MaybeRefOrGetter } from 'vue'
 import { projectService } from '@renderer/factory/service/projectService'
-import { customRef, toValue, watch } from 'vue'
-import type { Maybe, Nullable } from '@shared/lib/utils/types'
-import { computedAsync, toReactive, watchImmediate } from '@vueuse/core'
-import { check, isAbsent, isPresent } from '@shared/lib/utils/checks'
-import {
-  type EntityPublisher,
-  type EntityPublisherEvent,
-  getEntityChannel,
-} from '@shared/events/entityPublisher'
+import type { Maybe } from '@shared/lib/utils/types'
 import type { ProjectEntityDao, ProjectEntityDto } from '@shared/model/project'
-import type { SubscriberCallback } from '@shared/events/publisher'
-import { getOrDefault } from '@shared/lib/utils/result'
-import { entriesOf } from '@shared/lib/utils/object'
-import { toString } from '@shared/lib/utils/casting'
-
-function createSubscriber<TEntity extends object>(
-  fieldName: string,
-  onUpdate: (newEntity: Nullable<TEntity>) => void,
-): SubscriberCallback<EntityPublisherEvent<TEntity>> {
-  return (event) => {
-    if (event.type === 'deleted') {
-      onUpdate(null)
-    }
-
-    if (
-      event.type === 'updated' &&
-      event.changedFields.includes(fieldName as any as keyof TEntity)
-    ) {
-      onUpdate(event.data)
-    }
-  }
-}
-
-interface EntityFieldToRefOptions<
-  TEntity extends object,
-  TValue extends unknown,
-> {
-  fieldName: string
-  defaultValue: TValue
-  entityId: MaybeRefOrGetter<Maybe<string>>
-  initialEntity: Ref<Nullable<Readonly<TEntity>>>
-  publisher: EntityPublisher<TEntity>
-  onChanged: (newValue: TValue) => void
-}
-
-function entityFieldToRef<TEntity extends object, TValue extends unknown>(
-  options: EntityFieldToRefOptions<TEntity, TValue>,
-): Ref<TValue> {
-  const {
-    fieldName,
-    defaultValue,
-    entityId,
-    initialEntity,
-    publisher,
-    onChanged,
-  } = options
-
-  return customRef((track, trigger) => {
-    let value: TValue
-
-    function update(entity: Nullable<TEntity>) {
-      value = getOrDefault(
-        entity?.[fieldName as any as keyof TEntity] as TValue,
-        defaultValue,
-      )
-      trigger()
-    }
-
-    const subscriber = createSubscriber(fieldName, update)
-
-    // the entityId might change or not be present, so we need to dynamically subscribe and unsubscribe
-    watchImmediate(
-      () => toValue(entityId),
-      (newValue, oldValue) => {
-        // unsubscribe from the old entity
-        if (isPresent(oldValue)) {
-          publisher.unsubscribe(getEntityChannel(oldValue), subscriber)
-        }
-
-        // subscribe to the new entity
-        if (isPresent(newValue)) {
-          publisher.subscribe(getEntityChannel(newValue), subscriber)
-        }
-      },
-    )
-
-    // set the initial value
-    watch(initialEntity, (newValue) => {
-      update(newValue)
-    })
-
-    return {
-      get() {
-        track()
-        return value
-      },
-      set(newValue: TValue) {
-        onChanged(newValue)
-
-        // optimistic update
-        value = newValue
-        trigger()
-      },
-    }
-  })
-}
-
-interface EntityToRefsOptions<TIn extends TOut, TOut extends object> {
-  entityId: MaybeRefOrGetter<Maybe<string>>
-  publisher: EntityPublisher<TIn>
-  getterFn: (id: string) => Promise<Nullable<Readonly<TIn>>>
-  patchFn: (id: string, patch: Partial<TIn>) => Promise<Readonly<TIn>>
-  // the object returned will only contain fields that are present in the defaultValues object
-  defaultValues: Partial<TOut>
-}
-
-function entityToRefs<TIn extends TOut, TOut extends object>(
-  options: EntityToRefsOptions<TIn, TOut>,
-): {
-  [K in keyof TOut]: Ref<TOut[K]>
-} {
-  const { entityId, publisher, getterFn, patchFn, defaultValues } = options
-
-  const initialEntity = computedAsync(async () => {
-    const id = toValue(entityId)
-
-    if (isAbsent(id)) {
-      return null
-    }
-
-    return await getterFn(id)
-  }, null)
-
-  return entriesOf(defaultValues).reduce((acc, [fieldName, defaultValue]) => {
-    acc[fieldName] = entityFieldToRef({
-      fieldName: fieldName as any as string,
-      defaultValue,
-      entityId,
-      initialEntity,
-      publisher,
-      onChanged(newValue) {
-        const id = toValue(entityId)
-
-        check(
-          isPresent(id),
-          `Tried to update field ${toString(fieldName)} on entity dao with absent id`,
-        )
-
-        patchFn(
-          id,
-          // @ts-expect-error
-          {
-            [fieldName]: newValue,
-          },
-        )
-      },
-    })
-
-    return acc
-  }, {} as any)
-}
+import { createEntityDao } from '@renderer/lib/reactify/reactifyEntity'
 
 export function useProjectById(
   projectId: MaybeRefOrGetter<Maybe<string>>,
 ): ProjectEntityDao {
-  return toReactive(
-    entityToRefs<ProjectEntityDto, ProjectEntityDao>({
-      entityId: projectId,
-      publisher: projectService,
-      getterFn: projectService.getProjectById,
-      patchFn: projectService.patchProjectById,
-      defaultValues: {
-        id: null,
-        displayName: '',
-        color: null,
-        isBillable: false,
-        createdAt: null,
-        modifiedAt: null,
-        deletedAt: null,
-      },
-    }),
-  )
+  return createEntityDao<ProjectEntityDto, ProjectEntityDao>({
+    entityId: projectId,
+    publisher: projectService,
+    getterFn: projectService.getProjectById,
+    patchFn: projectService.patchProjectById,
+    defaultValues: {
+      id: null,
+      displayName: '',
+      color: null,
+      isBillable: false,
+      createdAt: null,
+      modifiedAt: null,
+      deletedAt: null,
+    },
+  })
 }
---
+++ src/renderer/lib/reactify/reactifyEntity.ts
@@ -0,0 +1,174 @@
+import type { Maybe, Nullable } from '@shared/lib/utils/types'
+import type { SubscriberCallback } from '@shared/events/publisher'
+import {
+  type EntityPublisher,
+  type EntityPublisherEvent,
+  getEntityChannel,
+} from '@shared/events/entityPublisher'
+import { customRef, type MaybeRefOrGetter, type Ref, toValue, watch } from 'vue'
+import { getOrDefault } from '@shared/lib/utils/result'
+import { computedAsync, toReactive, watchImmediate } from '@vueuse/core'
+import { check, isAbsent, isPresent } from '@shared/lib/utils/checks'
+import { entriesOf } from '@shared/lib/utils/object'
+import { toString } from '@shared/lib/utils/casting'
+
+function createSubscriber<TEntity extends object>(
+  fieldName: string,
+  onUpdate: (newEntity: Nullable<TEntity>) => void,
+): SubscriberCallback<EntityPublisherEvent<TEntity>> {
+  return (event) => {
+    if (event.type === 'deleted') {
+      onUpdate(null)
+    }
+
+    if (
+      event.type === 'updated' &&
+      event.changedFields.includes(fieldName as any as keyof TEntity)
+    ) {
+      onUpdate(event.data)
+    }
+  }
+}
+
+export interface EntityFieldToRefOptions<
+  TEntity extends object,
+  TValue extends unknown,
+> {
+  fieldName: string
+  defaultValue: TValue
+  entityId: MaybeRefOrGetter<Maybe<string>>
+  initialEntity: Ref<Nullable<Readonly<TEntity>>>
+  publisher: EntityPublisher<TEntity>
+  onChanged: (newValue: TValue) => void
+}
+
+export function entityFieldToRef<
+  TEntity extends object,
+  TValue extends unknown,
+>(options: EntityFieldToRefOptions<TEntity, TValue>): Ref<TValue> {
+  const {
+    fieldName,
+    defaultValue,
+    entityId,
+    initialEntity,
+    publisher,
+    onChanged,
+  } = options
+
+  return customRef((track, trigger) => {
+    let value: TValue
+
+    function update(entity: Nullable<TEntity>) {
+      value = getOrDefault(
+        entity?.[fieldName as any as keyof TEntity] as TValue,
+        defaultValue,
+      )
+      trigger()
+    }
+
+    const subscriber = createSubscriber(fieldName, update)
+
+    // the entityId might change or not be present, so we need to dynamically subscribe and unsubscribe
+    watchImmediate(
+      () => toValue(entityId),
+      (newValue, oldValue) => {
+        // unsubscribe from the old entity
+        if (isPresent(oldValue)) {
+          publisher.unsubscribe(getEntityChannel(oldValue), subscriber)
+        }
+
+        // subscribe to the new entity
+        if (isPresent(newValue)) {
+          publisher.subscribe(getEntityChannel(newValue), subscriber)
+        }
+      },
+    )
+
+    // set the initial value
+    watch(initialEntity, (newValue) => {
+      update(newValue)
+    })
+
+    return {
+      get() {
+        track()
+        return value
+      },
+      set(newValue: TValue) {
+        onChanged(newValue)
+
+        // optimistic update
+        value = newValue
+        trigger()
+      },
+    }
+  })
+}
+
+export interface EntityToRefsOptions<TIn extends TOut, TOut extends object> {
+  entityId: MaybeRefOrGetter<Maybe<string>>
+  // used to subscribe to changes on the entity.
+  //  each field will subscribe individually
+  publisher: EntityPublisher<TIn>
+  // will be used to get the initial values of the entity, and will be called each time the entityId changes
+  getterFn: (id: string) => Promise<Nullable<Readonly<TIn>>>
+  // will get called each time a field is updated
+  patchFn: (id: string, patch: Partial<TIn>) => Promise<Readonly<TIn>>
+  // the default values object is used to find all fields that should exist in the output object
+  defaultValues: TOut
+}
+
+export function entityToRefs<TIn extends TOut, TOut extends object>(
+  options: EntityToRefsOptions<TIn, TOut>,
+): {
+  [K in keyof TOut]: Ref<TOut[K]>
+} {
+  const { entityId, publisher, getterFn, patchFn, defaultValues } = options
+
+  const initialEntity = computedAsync(async () => {
+    const id = toValue(entityId)
+
+    if (isAbsent(id)) {
+      return null
+    }
+
+    return await getterFn(id)
+  }, null)
+
+  return entriesOf(defaultValues).reduce((acc, [fieldName, defaultValue]) => {
+    acc[fieldName] = entityFieldToRef({
+      fieldName: fieldName as any as string,
+      defaultValue,
+      entityId,
+      initialEntity,
+      publisher,
+      onChanged(newValue) {
+        const id = toValue(entityId)
+
+        check(
+          isPresent(id),
+          `Tried to update field ${toString(fieldName)} on entity dao with absent id`,
+        )
+
+        patchFn(
+          id,
+          // @ts-expect-error shhhh it's fine
+          {
+            [fieldName]: newValue,
+          },
+        )
+      },
+    })
+
+    return acc
+  }, {} as any)
+}
+
+// creates a reactive object from an entity.
+//  the returned dao will stay in sync with the entity in the database and all other dao's of the same entity.
+//  all changes will be immediately patched to the database.
+export function createEntityDao<TIn extends TOut, TOut extends object>(
+  options: EntityToRefsOptions<TIn, TOut>,
+): TOut {
+  return toReactive(entityToRefs(options)) as TOut
+}
