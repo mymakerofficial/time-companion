5dda9d0d9db06fd8b1e2e3b7f2d3c8c1a2d6b893
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-05-12 15:27:21 +0200

    chore: removed unused imports

--- src/shared/database/factory/queryable.ts
+++ src/shared/database/factory/queryable.ts
@@ -4,13 +4,12 @@ import type {
   Queryable,
 } from '@shared/database/types/database'
 import type { DatabaseTableAdapter } from '@shared/database/types/adapter'
-import { getOrDefault, getOrNull } from '@shared/lib/utils/result'
-import { maybeUnwrapOrderBy } from '@shared/database/helpers/unwrapOrderBy'
+import { getOrNull } from '@shared/lib/utils/result'
 import type { Nullable } from '@shared/lib/utils/types'
 import { check, isDefined, isNull } from '@shared/lib/utils/checks'
 import { filteredIterator } from '@shared/database/factory/helpers/filteredIterator'
 import { cursorIterator } from '@shared/database/factory/helpers/cursorIterator'
-import { firstOf, firstOfOrNull } from '@shared/lib/utils/list'
+import { firstOfOrNull } from '@shared/lib/utils/list'
 
 export class DatabaseQueryableImpl<TData extends object>
   implements Queryable<TData>
