1386aa8df3ae63396f0dac80e942e053f5187ef7
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-05-30 19:40:18 +0200

    chore(idb): extracted open cursor and iterator

--- src/shared/database/adapters/indexedDB/table.ts
+++ src/shared/database/adapters/indexedDB/table.ts
@@ -8,25 +8,13 @@ import type {
   TableAdapter,
 } from '@shared/database/types/adapter'
 import type { Nullable } from '@shared/lib/utils/types'
-import { check, isNotNull, isNull, isUndefined } from '@shared/lib/utils/checks'
+import { isNotNull, isNull, isUndefined } from '@shared/lib/utils/checks'
 import {
-  DatabaseInvalidRangeColumnError,
   DatabaseUndefinedColumnError,
   DatabaseUndefinedTableError,
   DatabaseUniqueViolationError,
 } from '@shared/database/types/errors'
-import { arraysHaveOverlap, toArray } from '@shared/lib/utils/list'
-import { getOrDefault, getOrNull } from '@shared/lib/utils/result'
-import { filteredIterator } from '@shared/database/helpers/filteredIterator'
-import { cursorIterator } from '@shared/database/helpers/cursorIterator'
-import type {
-  KeyRange,
-  OrderByDirection,
-} from '@shared/database/types/database'
-import type { DatabaseCursor } from '@shared/database/types/cursor'
-import { IDBKeyRange } from 'fake-indexeddb'
-import { directionToIdbCursorDirection } from '@shared/database/adapters/indexedDB/helpers/directionToIdbCursorDirection'
-import { IndexedDBCursorImpl } from '@shared/database/adapters/indexedDB/helpers/cursor'
+import { arraysHaveOverlap } from '@shared/lib/utils/list'
 import {
   iteratorToList,
   iteratorToSortedList,
@@ -34,6 +22,7 @@ import {
 import type { TableSchemaRaw } from '@shared/database/types/schema'
 import { keysOf, valuesOf } from '@shared/lib/utils/object'
 import { promisedRequest } from '@shared/database/adapters/indexedDB/helpers/promisedRequest'
+import { openIterator } from '@shared/database/adapters/indexedDB/helpers/openIterator'
 
 export class IdbTableAdapter<TRow extends object>
   implements TableAdapter<TRow>
@@ -62,105 +51,8 @@ export class IdbTableAdapter<TRow extends object>
     return this._objectStore
   }
 
-  protected async openIterator(props: Partial<AdapterBaseQueryProps>) {
-    const indexes = toArray(this.objectStore.indexNames)
-    const primaryKey = this.objectStore.keyPath
-
-    // if the range or orderBy column is the primary key, make it null
-    //  we use null to indicate that we should not use an index
-    //  but use the object store itself
-
-    // undefined means there is actually no value
-
-    const rangeColumn =
-      props.range?.column.columnName !== primaryKey
-        ? props.range?.column.columnName
-        : null
-    const orderByColumn =
-      props.orderBy?.column.columnName !== primaryKey
-        ? props.orderBy?.column.columnName
-        : null
-
-    check(
-      isNull(rangeColumn) ||
-        isUndefined(rangeColumn) ||
-        indexes.includes(rangeColumn),
-      () => new DatabaseInvalidRangeColumnError(rangeColumn!),
-    )
-
-    // we prefer the range column over the orderBy column
-    //  if both are given, we need to sort manually later
-    const keyPath = rangeColumn ?? orderByColumn ?? null
-
-    const keyPathIsIndex = isNotNull(keyPath)
-      ? indexes.includes(keyPath)
-      : false
-
-    // we need to manually sort if a range is given that doesn't match the orderBy column
-    //  or if the orderBy column is not indexed
-    const requiresManualSort =
-      !keyPathIsIndex ||
-      (isNotNull(rangeColumn) && rangeColumn !== orderByColumn)
-
-    // null means we sort by primary key
-    const indexName = keyPathIsIndex ? keyPath : null
-
-    const direction = getOrDefault(props.orderBy?.direction, 'asc')
-
-    const where = getOrNull(props.where)
-    const range = getOrNull(props.range)
-    const limit = getOrDefault(props.limit, Infinity)
-    const offset = getOrDefault(props.offset, 0)
-
-    // we have now planned the query and can finally open the cursor
-
-    const cursor = await this.openCursor(indexName, direction, range)
-
-    return {
-      iterator: filteredIterator(cursorIterator(cursor), where, limit, offset),
-      byColumn: orderByColumn as Nullable<keyof TRow>,
-      direction,
-      requiresManualSort,
-    }
-  }
-
-  protected openCursor(
-    indexName: Nullable<string>,
-    direction: OrderByDirection,
-    keyRange: Nullable<KeyRange>,
-  ): Promise<DatabaseCursor<TRow>> {
-    return new Promise((resolve, reject) => {
-      const indexOrObjectStore = isNotNull(indexName)
-        ? this.objectStore.index(indexName)
-        : this.objectStore
-
-      const range = isNotNull(keyRange)
-        ? IDBKeyRange.bound(
-            // turn the key range into a key range... but different
-            keyRange.lower,
-            keyRange.upper,
-            keyRange.lowerOpen,
-            keyRange.upperOpen,
-          )
-        : null
-
-      const cursorDirection = directionToIdbCursorDirection(direction)
-
-      const request = indexOrObjectStore.openCursor(range, cursorDirection)
-
-      request.onsuccess = () => {
-        const cursor = new IndexedDBCursorImpl<TRow>(
-          request,
-          this.objectStore.keyPath as keyof TRow,
-        )
-
-        resolve(cursor)
-      }
-
-      request.onerror = () => {
-        reject(request.error)
-      }
-    })
+  protected openIterator(props: Partial<AdapterBaseQueryProps>) {
+    return openIterator<TRow>(this.objectStore, props)
   }
 
   async select(props: AdapterSelectProps<TRow>): Promise<Array<TRow>> {
