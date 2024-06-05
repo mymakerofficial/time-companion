5bfd1cf32b01ceee3fff7392898c26d076d2ac8c
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-06-05 20:46:59 +0200

    chore: removed dutch and cat

--- src/renderer/locales/locales.ts
+++ src/renderer/locales/locales.ts
@@ -1,8 +1,6 @@
 import { createI18n } from 'vue-i18n'
 import enUs from './en-Us'
 import deInformal from './de-Informal'
-import nlNl from './nl-Nl'
-import cat from './cat'
 
 export const i18n = createI18n({
   locale: 'en-US',
@@ -12,7 +10,5 @@ export const i18n = createI18n({
   messages: {
     'en-US': enUs,
     'de-Informal': deInformal,
-    'nl-NL': nlNl,
-    cat: cat,
   },
 })
