808a1b5ce89f98abe4e66c33c6e428c87c785117
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-06-06 21:07:40 +0200

    feat: added useGetTaskById query

---
+++ src/renderer/composables/queries/tasks/useGetTaskById.ts
@@ -0,0 +1,34 @@
+import { useQuery, useQueryClient } from '@tanstack/vue-query'
+import type { MaybeRef } from 'vue'
+import { toValue, watchEffect } from 'vue'
+import { check, isUndefined } from '@shared/lib/utils/checks'
+import { taskService } from '@renderer/factory/service/taskService'
+
+export function useGetTaskById(id: MaybeRef<string>) {
+  const queryClient = useQueryClient()
+  const queryKey = ['tasks', id]
+
+  watchEffect(() => {
+    if (isUndefined(queryClient.getQueryData(queryKey))) {
+      return
+    }
+
+    taskService.subscribe(
+      {
+        entityId: toValue(id),
+        type: 'updated',
+      },
+      (event) => {
+        check(event.type === 'updated')
+        queryClient.setQueryData(queryKey, event.data)
+      },
+    )
+
+    // we don't need to unsubscribe because somebody else might use the same query again
+  })
+
+  return useQuery({
+    queryKey,
+    queryFn: () => taskService.getTaskById(toValue(id)),
+  })
+}
