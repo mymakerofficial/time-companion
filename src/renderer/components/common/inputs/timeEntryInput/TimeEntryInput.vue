fee021a0b96c9b9a3e2e4070feff148893970eb2
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-06-10 17:44:34 +0200

    feat: add new time entry input component

---
+++ src/renderer/components/common/inputs/timeEntryInput/TimeEntryInput.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Input } from '@renderer/components/ui/input'
+import ProjectSelect from '@renderer/components/common/inputs/projectInput/ProjectSelect.vue'
+import type { Nullable } from '@shared/lib/utils/types'
+import type { ProjectDto } from '@shared/model/project'
+
+const project = defineModel<Nullable<ProjectDto>>('project', { required: true })
+const description = defineModel<string>('description', { required: true })
+</script>
+
+<template>
+  <div class="flex flex-row gap-2">
+    <ProjectSelect v-model="project" allow-deselect />
+    <Input
+      v-model="description"
+      :placeholder="$t('dashboard.labels.whatAreYouWorkingOn')"
+    />
+  </div>
+</template>
--- src/renderer/views/PlaygroundView.vue
+++ src/renderer/views/PlaygroundView.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
 import ResponsiveContainer from '@renderer/components/common/layout/ResponsiveContainer.vue'
-import ProjectsTableSection from '@renderer/components/playground/ProjectsTableSection.vue'
-import CreateProjectSection from '@renderer/components/playground/CreateProjectSection.vue'
-import EditProjectSection from '@renderer/components/playground/EditProjectSection.vue'
-import { ref } from 'vue'
-import type { Nullable } from '@shared/lib/utils/types'
-
-const selectedProjectId = ref<Nullable<string>>(null)
+import SettingsSection from '@renderer/components/settings/layout/SettingsSection.vue'
+import TimeEntryInput from '@renderer/components/common/inputs/timeEntryInput/TimeEntryInput.vue'
 </script>
 
 <template>
   <ResponsiveContainer class="my-14 flex flex-col">
-    <CreateProjectSection />
-    <ProjectsTableSection
-      @edit="(project) => (selectedProjectId = project.id)"
-    />
-    <EditProjectSection :project-id="selectedProjectId" />
+    <SettingsSection>
+      <TimeEntryInput />
+    </SettingsSection>
   </ResponsiveContainer>
 </template>
