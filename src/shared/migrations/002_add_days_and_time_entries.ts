6bde966118dd5e7320e5b8dba6fde9f4aee813cd
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-06-17 17:44:04 +0200

    fix: mistake in migration

--- src/shared/migrations/002_add_days_and_time_entries.ts
+++ src/shared/migrations/002_add_days_and_time_entries.ts
@@ -5,7 +5,7 @@ export default defineMigration(async (transaction) => {
   await transaction.createTable('days', {
     id: c.uuid().primaryKey(),
     date: c.date().indexed().unique(),
-    targetWorkDuration: c.interval().nullable(),
+    targetBillableDuration: c.interval().nullable(),
     createdAt: c.datetime(),
     modifiedAt: c.datetime().nullable(),
     deletedAt: c.datetime().nullable(),
