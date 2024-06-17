324d0d4291956ab069007917ab286ab0e76da767
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-06-17 17:54:17 +0200

    feat: added time entry checks

--- src/shared/persistence/timeEntryPersistence.ts
+++ src/shared/persistence/timeEntryPersistence.ts
@@ -6,13 +6,16 @@ import {
   type UpdateTimeEntry,
 } from '@shared/model/timeEntry'
 import {
-  toTimeEntryDto,
-  timeEntryEntityUpdateFrom,
   timeEntryEntityCreateFrom,
+  timeEntryEntityUpdateFrom,
+  toTimeEntryDto,
 } from '@shared/model/mappers/timeEntry'
 import { PlainDateTime } from '@shared/lib/datetime/plainDateTime'
 import { uuid } from '@shared/lib/utils/uuid'
 import { firstOf } from '@shared/lib/utils/list'
+import { daysTable } from '@shared/model/day'
+import { toDayDto } from '@shared/model/mappers/day'
+import { check, isNotNull } from '@shared/lib/utils/checks'
 
 export type TimeEntryPersistenceDependencies = {
   database: Database
@@ -68,12 +71,43 @@ class TimeEntryPersistenceImpl implements TimeEntryPersistence {
   }
 
   async createTimeEntry(timeEntry: CreateTimeEntry): Promise<TimeEntryDto> {
-    return await this.database.table(timeEntriesTable).insert({
-      data: timeEntryEntityCreateFrom(timeEntry, {
-        id: uuid(),
-        createdAt: PlainDateTime.now(),
-      }),
-      map: toTimeEntryDto,
+    return await this.database.withTransaction(async (tx) => {
+      const day = await tx.table(daysTable).findFirst({
+        range: daysTable.id.range.only(timeEntry.dayId),
+        map: toDayDto,
+      })
+
+      check(isNotNull(day), `Day with id "${timeEntry.dayId}" not found`)
+
+      check(
+        timeEntry.startedAt.isAfter(day.date),
+        `Time entry must start after midnight of the given day.`,
+      )
+
+      const earliestTimeEntry = await tx.table(timeEntriesTable).findFirst({
+        range: timeEntriesTable.dayId.range.only(day.id),
+        orderBy: timeEntriesTable.startedAt.asc(),
+        map: toTimeEntryDto,
+      })
+
+      if (isNotNull(earliestTimeEntry)) {
+        check(
+          earliestTimeEntry.startedAt
+            .add({ hours: 24 })
+            .isAfter(timeEntry.stoppedAt ?? timeEntry.startedAt),
+          `Time entry must end at most 24 hours after the first time entry of the given day.`,
+        )
+
+        // TODO check if time entry overlaps with existing time entries
+      }
+
+      return await tx.table(timeEntriesTable).insert({
+        data: timeEntryEntityCreateFrom(timeEntry, {
+          id: uuid(),
+          createdAt: PlainDateTime.now(),
+        }),
+        map: toTimeEntryDto,
+      })
     })
   }
 
