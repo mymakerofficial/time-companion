42c6d4b765b5c13f04c85297cadc63750dc40eb8
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-06-23 21:41:50 +0200

    added running time entry input

---
+++ src/renderer/components/common/inputs/timeEntryInput/RunningTimeEntryInput.vue
@@ -0,0 +1,128 @@
+<script setup lang="ts">
+import TimeEntryInput from '@renderer/components/common/inputs/timeEntryInput/TimeEntryInput.vue'
+import DateTimeInput from '@renderer/components/common/inputs/timeInput/DateTimeInput.vue'
+import { Button } from '@renderer/components/ui/button'
+import { useGetRunningTimeEntry } from '@renderer/composables/queries/timeEntries/useGetRunningTimeEntry'
+import { useCreateTimeEntry } from '@renderer/composables/mutations/timeEntries/useCreateTimeEntry'
+import { computed, ref, watch } from 'vue'
+import { isNotNull, isNull } from '@shared/lib/utils/checks'
+import { usePatchTimeEntry } from '@renderer/composables/mutations/timeEntries/usePatchTimeEntry'
+import { useI18n } from 'vue-i18n'
+import { PlainDateTime } from '@shared/lib/datetime/plainDateTime'
+import type { Nullable } from '@shared/lib/utils/types'
+import { useNow } from '@renderer/composables/useNow'
+import { humanizeDuration } from '@renderer/lib/neoTime'
+
+const props = defineProps<{
+  dayId: string
+}>()
+
+const { t } = useI18n()
+const now = useNow()
+const { data: runningEntry } = useGetRunningTimeEntry(props.dayId)
+const { mutateAsync: createEntry } = useCreateTimeEntry()
+const { mutateAsync: patchEntry } = usePatchTimeEntry()
+
+const projectId = ref<Nullable<string>>(null)
+const description = ref('')
+const startedAt = ref(PlainDateTime.now())
+
+watch(runningEntry, (entry) => {
+  if (isNull(entry)) {
+    projectId.value = null
+    description.value = ''
+    startedAt.value = PlainDateTime.now()
+    return
+  }
+  projectId.value = entry.projectId
+  description.value = entry.description
+  startedAt.value = entry.startedAt
+})
+
+const isRunning = computed(() => isNotNull(runningEntry.value))
+
+const buttonLabel = computed(() => {
+  if (isRunning.value) {
+    return t('dashboard.controls.stopEvent')
+  } else {
+    return t('dashboard.controls.startEvent')
+  }
+})
+
+const duration = computed(() => {
+  return startedAt.value.until(now.value)
+})
+
+const durationLabel = computed(() => {
+  return humanizeDuration(duration.value, {
+    includeSeconds: true,
+  })
+})
+
+async function handleChange() {
+  if (isNull(runningEntry.value)) return
+  console.log(startedAt.value.toString())
+  await patchEntry({
+    id: runningEntry.value.id,
+    timeEntry: {
+      projectId: projectId.value,
+      description: description.value,
+      startedAt: startedAt.value,
+    },
+  })
+}
+
+async function handleStart() {
+  await createEntry({
+    projectId: projectId.value,
+    description: description.value,
+    startedAt: PlainDateTime.now(),
+    dayId: props.dayId,
+  })
+}
+
+async function handleStop() {
+  if (isNull(runningEntry.value)) return
+  await patchEntry({
+    id: runningEntry.value.id,
+    timeEntry: {
+      stoppedAt: PlainDateTime.now(),
+    },
+  })
+}
+
+async function handleToggle() {
+  if (isRunning.value) {
+    await handleStop()
+  } else {
+    await handleStart()
+  }
+}
+</script>
+
+<template>
+  <TimeEntryInput
+    v-model:project-id="projectId"
+    v-model:description="description"
+    @change="handleChange"
+  >
+    <template #leading>
+      <slot name="leading" />
+    </template>
+    <template #trailing>
+      <Button @click="handleToggle">{{ buttonLabel }}</Button>
+      <DateTimeInput
+        v-if="isRunning"
+        v-model="startedAt"
+        @change="handleChange"
+        class="w-fit"
+      />
+      <time
+        v-if="isRunning"
+        class="text-lg font-medium text-center min-w-24"
+        v-text="durationLabel"
+      />
+      <slot name="trailing" />
+    </template>
+  </TimeEntryInput>
+</template>
