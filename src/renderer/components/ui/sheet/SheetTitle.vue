2f274eb767cd2dadf41d963d2008f57d85253ea8
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-06-24 20:58:09 +0200

    chore: added Sheet components

---
+++ src/renderer/components/ui/sheet/SheetTitle.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed, type HTMLAttributes } from 'vue'
+import { DialogTitle, type DialogTitleProps } from 'radix-vue'
+import { cn } from '@renderer/lib/utils'
+
+const props = defineProps<
+  DialogTitleProps & { class?: HTMLAttributes['class'] }
+>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+</script>
+
+<template>
+  <DialogTitle
+    :class="cn('text-lg font-semibold text-foreground', props.class)"
+    v-bind="delegatedProps"
+  >
+    <slot />
+  </DialogTitle>
+</template>
