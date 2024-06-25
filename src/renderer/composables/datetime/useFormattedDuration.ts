dbf512593bf174af60fdade6715a9307bfe26d8e
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-06-25 15:16:28 +0200

    fix: wrong options

--- src/renderer/composables/datetime/useFormattedDuration.ts
+++ src/renderer/composables/datetime/useFormattedDuration.ts
@@ -1,15 +1,14 @@
 import { computed, type MaybeRefOrGetter, toValue } from 'vue'
 import type { Maybe } from '@shared/lib/utils/types'
-import type { Intl } from 'temporal-polyfill'
 import { humanizeDuration } from '@renderer/lib/neoTime'
 import type { Duration } from '@shared/lib/datetime/duration'
 import { isAbsent } from '@shared/lib/utils/checks'
 
 export function useFormattedDuration(
   duration: MaybeRefOrGetter<Maybe<Duration>>,
-  options: Intl.DateTimeFormatOptions & { fallback?: string } = {},
+  options: { includeSeconds?: boolean; fallback?: string } = {},
 ) {
-  const { fallback = '', ...temporalOptions } = options
+  const { fallback = '', ...humanizeOptions } = options
 
   return computed(() => {
     const unrefDuration = toValue(duration)
@@ -18,6 +17,6 @@ export function useFormattedDuration(
       return fallback
     }
 
-    return humanizeDuration(unrefDuration)
+    return humanizeDuration(unrefDuration, humanizeOptions)
   })
 }
