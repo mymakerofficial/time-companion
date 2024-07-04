19f15b86bf68ab3cb272fb71bb8d969ff3d75736
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-04 21:28:03 +0200

    feat: enable foreign key constraints

--- src/shared/drizzle/init.ts
+++ src/shared/drizzle/init.ts
@@ -1,20 +1,28 @@
 import type { BaseSQLiteDatabase } from 'drizzle-orm/sqlite-core/db'
 import type { SQLiteWasmDatabase } from '@shared/drizzle/sqlite-wasm/driver'
 import { isFunction } from '@shared/lib/utils/checks'
+import { sql } from 'drizzle-orm'
 
 /***
- * Initializes the database if required.
+ * Initializes the database.
+ *  This function must be run before any other database operations.
  */
 export async function initialize<
   TSchema extends Record<string, unknown> = Record<string, never>,
 >(database: BaseSQLiteDatabase<'sync', unknown, TSchema>) {
   if (isSQLiteWasmDatabase(database)) {
+    // sqlite-wasm requires explicit initialization
+    //  this has to be async
     await database.init()
   }
+  // enable foreign key constraints
+  database.run(sql`PRAGMA foreign_keys = ON`)
 }
 
-function isSQLiteWasmDatabase(
-  database: any,
-): database is SQLiteWasmDatabase<{}, {}> {
-  return isFunction(database?.init)
+function isSQLiteWasmDatabase<
+  TSchema extends Record<string, unknown> = Record<string, never>,
+>(
+  database: BaseSQLiteDatabase<'sync', unknown, TSchema>,
+): database is SQLiteWasmDatabase<TSchema> {
+  return isFunction((database as SQLiteWasmDatabase<TSchema>).init)
 }
